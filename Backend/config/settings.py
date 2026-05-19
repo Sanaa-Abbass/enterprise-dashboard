02e24b2c394b10c2d442d8be203d30b0ceb9723c
Sanaa-Abbas <ssmohamedahmedaabbas@gmail.com>
2026-05-19 12:22:33 +0400

    Prepare Django backend for Render deployment

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!4%&&e@-t97=fk_=v$zfm6*m+crt=njbf-ck5w6kug6xa1hzyq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
