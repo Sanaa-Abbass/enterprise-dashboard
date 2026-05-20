c36b77131c56c6e0461a650737383f8ad4d81b3f
Sanaa-Abbas <ssmohamedahmedaabbas@gmail.com>
2026-05-20 11:44:42 +0400

    Enable CORS and production API config

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -131,6 +131,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
